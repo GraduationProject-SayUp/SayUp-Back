f9266cead252228c751b9a44e3f7ea9db2f53cb0
Kim-YuKyung <kimyk1116@naver.com>
2024-12-13 19:54:05 +0900

    Initial commit for SayUp project

---
+++ src/main/java/com/sayup/SayUp/SayUpApplication.java
@@ -0,0 +1,13 @@
+package com.sayup.SayUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayup/SayUp/SayUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayup.SayUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
