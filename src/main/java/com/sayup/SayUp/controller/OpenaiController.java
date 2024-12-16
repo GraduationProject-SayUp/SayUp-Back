591b3968f997e06597a24b18b6e5416065702d87
Kim-YuKyung <kimyk1116@naver.com>
2024-12-16 18:26:30 +0900

    목소리 변환 API 보안 설정 변경

--- src/main/java/com/sayup/SayUp/controller/OpenaiController.java
+++ src/main/java/com/sayup/SayUp/controller/OpenaiController.java
@@ -1,7 +1,11 @@
 package com.sayup.SayUp.controller;
 
+import com.sayup.SayUp.security.JwtTokenProvider;
 import com.sayup.SayUp.service.OpenaiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,14 +16,37 @@ import java.util.Map;
 public class OpenaiController {
 
     private final OpenaiService openAIService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserTTSVectorController.class);
 
     @Autowired
-    public OpenaiController(OpenaiService openAIService) {
+    public OpenaiController(OpenaiService openAIService, JwtTokenProvider jwtTokenProvider) {
         this.openAIService = openAIService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    @PostMapping("/generate")
-    public ResponseEntity<String> generateChatResponse(@RequestParam("message") String userMessage) {
+    @PostMapping(value = "/generate",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> generateChatResponse(
+            @RequestHeader("Authorization") String token,
+            @RequestBody String userMessage) {
+
+        logger.info("Received Token: " + token);
+
+
+        // 토큰 값 검증
+        if (token.startsWith("Bearer ")) {
+            token = token.substring(7); // "Bearer " 제거
+        }
+
+        // 토큰이 유효한지 검증
+        if (!jwtTokenProvider.validateToken(token)) {
+            return ResponseEntity.status(401).body("Invalid or expired token");
+        }
+
+        // 토큰 검증 후 사용자 메시지 처리
         String content = openAIService.getChatResponse(userMessage);
         return ResponseEntity.ok(content); // content 값만 반환
     }
